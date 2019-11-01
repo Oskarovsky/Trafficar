97b1fb7e453efe8d8e028fc5766f1cd167e7fd4d
oskarro <oskar.slyk@gmail.com>
2019-11-01 23:17:36 +0100

    initialize individual project app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hell2osss 22!'
+
+@app.route("/about")
+def about():
+    return "<h1>All about trafficar</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
