c73f7a0951b6f48a9f464a1e864edde1c4fa2fba
oskarro <oskar.slyk@gmail.com>
2019-11-12 20:27:07 +0100

    first session

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,26 @@
 from .entities.entity import Session, engine, Base
 from .entities.box import Box
 
-# generate database schema
+# generate database schema (if needed)
 Base.metadata.create_all(engine)
 
 # start session
 session = Session()
 
-# check for existing data
+# check for existing data (it queries all instances of Box)
 boxes = session.query(Box).all()
 
 if len(boxes) == 0:
     # create and persist dummy box
-    python_box = Box("Bike", "Sport bike for professional cyclist", )
+    bike_box = Box("Bike", "Sport bike for professional cyclist", 1210, 430, 222, 120, 'Oskar Slyk')
+    session.add(bike_box)
+    session.commit()
+    session.close()
+
+    # reload boxes
+    boxes = session.query(Box).all()
+
+# show existing boxes
+print('### Boxes:')
+for box in boxes:
+    print(f'({box.id} {box.name} - {box.description} // PARAMETERS: {box.weight}, {box.width}, {box.height}, {box.length}')
