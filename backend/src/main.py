0ea848d1de7c84cded2dca1921cb41844712b528
oskarro <oskar.slyk@gmail.com>
2019-11-12 22:49:48 +0100

    integrating flask restful

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,26 +1,65 @@
+from flask import Flask, jsonify, request
+
 from .entities.entity import Session, engine, Base
-from .entities.box import Box
+from .entities.box import Box, BoxSchema
+
+
+# creating the Flask application
+app = Flask(__name__)
 
 # generate database schema (if needed)
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data (it queries all instances of Box)
-boxes = session.query(Box).all()
+@app.route('/boxes')
+def get_boxes():
+    # fetching from the database
+    session = Session()
+    box_objects = session.query(Box).all()
 
-if len(boxes) == 0:
-    # create and persist dummy box
-    bike_box = Box("Bike", "Sport bike for professional cyclist", 1210, 430, 222, 120, 'Oskar Slyk')
-    session.add(bike_box)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = BoxSchema(many=True)
+    boxes = schema.dump(box_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(boxes)
 
-    # reload boxes
-    boxes = session.query(Box).all()
 
-# show existing boxes
-print('### Boxes:')
-for box in boxes:
-    print(f'({box.id} {box.name} - {box.description} // PARAMETERS: {box.weight}, {box.width}, {box.height}, {box.length}')
+@app.route('/boxes', methods=['POST'])
+def add_box():
+    # mount exam object
+    posted_box = BoxSchema(only=('name', 'description', 'weight', 'width', 'height', 'length'))\
+        .load(request.get_json())
+
+    box = Box(**posted_box, created_by='HTTP post request')
+
+    # persist box
+    session = Session()
+    session.add(box)
+    session.commit()
+
+    # return created box
+    new_box = BoxSchema().dump(box)
+    session.close()
+    return jsonify(new_box), 201
+
+
+
+# # check for existing data (it queries all instances of Box)
+# boxes = session.query(Box).all()
+#
+# if len(boxes) == 0:
+#     # create and persist dummy box
+#     bike_box = Box("Bike", "Sport bike for professional cyclist", 1210, 430, 222, 120, 'Oskar Slyk')
+#     session.add(bike_box)
+#     session.commit()
+#     session.close()
+#
+#     # reload boxes
+#     boxes = session.query(Box).all()
+#
+# # show existing boxes
+# print('### Boxes:')
+# for box in boxes:
+#     print(f'({box.id} {box.name} - {box.description} // PARAMETERS: {box.weight}, {box.width}, {box.height}, {box.length}')
